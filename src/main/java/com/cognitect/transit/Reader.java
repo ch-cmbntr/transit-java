55a9826b1b842a7df4e42ed6b95a6c8f8f76386b
Michael Locher <cmbntr@gmail.com>
2024-07-23 15:28:47 +0200

    feat: allow read without EOFException

--- src/main/java/com/cognitect/transit/Reader.java
+++ src/main/java/com/cognitect/transit/Reader.java
@@ -14,6 +14,9 @@
 
 package com.cognitect.transit;
 
+import java.io.EOFException;
+import java.util.function.Predicate;
+
 /**
  * Interface for reading values in transit format
  */
@@ -23,4 +26,39 @@ public interface Reader {
      * @return the value
      */
     <T> T read();
+
+    /**
+     * Reads a single value from an input source
+     * @param eofValue the value if EOF is reached
+     * @return the value
+     */
+    default <T> T read(final T eofValue) {
+        try {
+            return read();
+        } catch (final RuntimeException e) {
+            if (e.getCause() instanceof EOFException) {
+                return eofValue;
+            }
+            throw e;
+        }
+    }
+
+    /**
+     * Reads a single value from an input source
+     * @param consumeAndContinue consumes the value and tells if the next value shall be read
+     */
+    default <T> void readWhile(final Predicate<? super T> consumeAndContinue) {
+        @SuppressWarnings("unchecked")
+        final T eof = (T) new Object();
+        T val;
+        boolean cont;
+        do {
+            val = read(eof);
+            if (val == eof) {
+                return;
+            }
+            cont = consumeAndContinue.test(eof);
+        } while (cont);
+    }
+
 }
